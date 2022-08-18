9c57882ed61e620b28dfa79308b1641b19c50dc8
Vit0rg <vgois032@gmail.com>
2022-08-18 18:40:13 -0300

    ForgotPassword and SignUp implemented

--- script.js
+++ script.js
@@ -4,15 +4,73 @@ const passwordInput = document.querySelector("[data-password-input]")
 const emailInput = document.querySelector("[data-email-input]")
 const forgotPasswordButton = document.querySelector("[data-forgot-password-button]")
 const signUp = document.querySelector("[data-sign-up-button]")
-const submitButton = document.querySelector("[data-submit-button]")
+const signInButton = document.querySelector("[data-sign-in-button]")
+const recoverButton = document.querySelector("[data-recover-button]")
+const repeatPasswordInput = document.querySelector("[data-repeat-password-input]")  
 
-forgotPasswordButton.addEventListener("click", () =>
+function hiddenToggler(arrayToToggle, arrayNewToggleState)
+{
+  for (let i = 0; i < arrayToToggle.length; i++) 
+  {
+    arrayToToggle[i].classList.toggle("hidden", arrayNewToggleState[i]);
+  }
+}
+
+forgotPasswordButton.addEventListener("click", loadForgotPasswordContent)
+
+function loadForgotPasswordContent()
 {
   header.innerText = "Recover password"
-  submitButton.value = "Send code"
-  emailInput.classList.remove("hidden")
-  forgotPasswordButton.classList.add("hidden")
-  usernameInput.classList.add("hidden")
-  passwordInput.classList.add("hidden")
-  signUp.classList.add("hidden")
+
+  const elementsToToggle = [emailInput, forgotPasswordButton, usernameInput,
+                            passwordInput, signUp, signInButton, recoverButton,
+                            repeatPasswordInput]
+  const newElementsState = [false, true, true, true, false, true, false, true]
+  hiddenToggler(elementsToToggle, newElementsState)
+  signUp.classList.remove("submit-button")
+}
+
+recoverButton.addEventListener("click", loadLoginContent
+)
+
+function loadLoginContent()
+{
+  signUp.removeEventListener("click", loadLoginContent)
+
+  header.innerText = "Login"
+  const elementsToToggle = [usernameInput, passwordInput, repeatPasswordInput,
+                            signInButton, emailInput, forgotPasswordButton,
+                            signInButton, recoverButton]
+  const newElementsState = [false, false, true, false, true, false, false, true]
+  hiddenToggler(elementsToToggle, newElementsState)
+  signUp.classList.remove("submit-button")
+}
+
+signUp.addEventListener("click", loadNewUserContent)
+
+function loadNewUserContent()
+{
+  header.innerText = "Create account"
+  const elementsToToggle = [usernameInput, passwordInput, repeatPasswordInput,
+                            signInButton, emailInput, forgotPasswordButton,
+                            signUp, recoverButton]
+  const newElementsState = [false, false, false, true, false, false, false, true]
+  hiddenToggler(elementsToToggle, newElementsState)
+  signUp.classList.add("submit-button")
+
+  signUp.addEventListener("click", () =>
+  {
+    alert("User registered")
+    loadLoginContent
+  })
+}
+
+signInButton.addEventListener("click", ()=>
+{
+  alert("Welcome")
+})
+
+recoverButton.addEventListener("click", () =>
+{
+  alert("Chek your e-mail")
 })
